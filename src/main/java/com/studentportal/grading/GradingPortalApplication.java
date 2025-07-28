107e76c076446d640478b4f45158ac3e56a008a5
DevObaloluwa <securedbyoba@gmail.com>
2025-07-29 00:04:31 +0100

    Initial Spring Boot setup

---
+++ src/main/java/com/studentportal/grading/GradingPortalApplication.java
@@ -0,0 +1,13 @@
+package com.studentportal.grading;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GradingPortalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GradingPortalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/studentportal/grading/GradingPortalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.studentportal.grading;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GradingPortalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
